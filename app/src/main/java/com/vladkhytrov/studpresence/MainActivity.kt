a509f90dd2cde24243dd49a62c7a45cd89ae69a2
Vlad Khytrov <vkhytrov@engagepeople.com>
2021-12-09 00:38:30 +0100

    registration screen

--- app/src/main/java/com/vladkhytrov/studpresence/MainActivity.kt
+++ app/src/main/java/com/vladkhytrov/studpresence/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.vladkhytrov.studpresence
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
+import com.google.zxing.integration.android.IntentIntegrator
+import com.vladkhytrov.studpresence.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+        binding.scan.setOnClickListener {
+            onScanClick()
+        }
+    }
+
+    private fun onScanClick() {
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(
+            listOf(IntentIntegrator.QR_CODE)
+        )
+        integrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val result = IntentIntegrator.parseActivityResult(requestCode, data)
+        if (result != null) {
+            binding.text.text = result.contents
+        }
     }
 }
